660e199433750a838093b33217a35f9ddcc90512
Rizky Aditya Ichwanto <44856377+radityaIch@users.noreply.github.com>
2022-08-28 13:57:44 +0800

    Change parameter for create and edit

--- models/TodoList.js
+++ models/TodoList.js
@@ -1,51 +1,50 @@
 import connector from "../db/connector.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 const db = await connector();
 const table = "Todos";
 
 export default class TodoList {
-  static async create(data) {
-    const sql = `
+	static async create(tag_id, title, description) {
+		const sql = `
 			INSERT INTO Todos(id, tag_id, title, description)
-			VALUES ("${uuidv4()}", "${data.tag_id}", "${data.title}", "${data.description}")
+			VALUES ("${uuidv4()}", "${tag_id}", "${title}", "${description}")
 		`;
 		const [rows] = await db.execute(sql);
-		return rows
-  }
+		return rows;
+	}
 
-  static async edit(newData) {
+	static async edit(id, tag_id, title, description) {
 		const sql = `
-      UPDATE ${table} SET title = "${newData.title}", description = "${newData.description}",
-      tag_id = "${newData.tag_id}" WHERE id = "${newData.id}"
+      UPDATE ${table} SET title = "${title}", description = "${description}",
+      tag_id = "${tag_id}" WHERE id = "${id}"
     `;
-    const [rows] = await db.execute(sql);
+		const [rows] = await db.execute(sql);
 		return rows;
 	}
 
 	static async delete(id) {
-    const sql = `
+		const sql = `
       DELETE FROM ${table} WHERE id = "${id}"
     `;
-    const [rows] = await db.execute(sql);
+		const [rows] = await db.execute(sql);
 		return rows;
-  }
+	}
 
-  static async find() {
-    const sql = `SELECT * FROM ${table}`;
-    const [rows] = await db.execute(sql);
-    return rows;
-  }
+	static async find() {
+		const sql = `SELECT * FROM ${table}`;
+		const [rows] = await db.execute(sql);
+		return rows;
+	}
 
-  static async findOneById(value) {
-    const sql = `SELECT * FROM ${table} WHERE id = "${value}"`;
-    const [rows] = await db.execute(sql);
-    return rows.length === 0 ? null : rows[0];
-  }
+	static async findOneById(value) {
+		const sql = `SELECT * FROM ${table} WHERE id = "${value}"`;
+		const [rows] = await db.execute(sql);
+		return rows.length === 0 ? null : rows[0];
+	}
 
-  static async search(field, value) {
-    const sql = `SELECT * FROM ${table} WHERE ${field} LIKE '%${value}%'`;
-    const [rows] = await db.execute(sql);
-    return rows;
-  }
+	static async search(field, value) {
+		const sql = `SELECT * FROM ${table} WHERE ${field} LIKE '%${value}%'`;
+		const [rows] = await db.execute(sql);
+		return rows;
+	}
 }
-
