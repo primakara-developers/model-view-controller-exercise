76581c4eca58f7c2c9b022039d70c8ccd7b975ca
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2022-08-28 14:23:53 +0800

    Pull upstream main

--- models/TagTodo.js
+++ models/TagTodo.js
@@ -4,25 +4,25 @@ const db = await connector();
 const table = "Tags";
 
 export default class TagTodo {
-	static create(todo, description, tag) {
-		this.findOne()
-	}
+  static create(todo, description, tag) {
+    this.findOne();
+  }
 
-	static edit() {}
+  static edit() {}
 
-	static delete() {}
+  static delete() {}
 
-	static async find() {
-		const sql = `SELECT * FROM ${table}`;
-		const [rows] = await db.execute(sql)
+  static async find() {
+    const sql = `SELECT * FROM ${table}`;
+    const [rows] = await db.execute(sql);
 
-		return rows;
-	}
+    return rows;
+  }
 
-	static async findOne(field, value) {
-		const sql = `SELECT * FROM ${table} WHERE ${field} LIKE '%${value}%'`;
-		const [rows] = await db.execute(sql)
+  static async findOne(field, value) {
+    const sql = `SELECT * FROM ${table} WHERE ${field} LIKE '%${value}%'`;
+    const [rows] = await db.execute(sql);
 
-		return rows;
-	}
+    return rows;
+  }
 }
