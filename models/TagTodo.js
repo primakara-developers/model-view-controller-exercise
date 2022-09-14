c86f880a6136e56e250354c13ce2a6f3a5e70020
Dipadana Putu <dipadana@Dipadana-PC.localdomain>
2022-09-14 11:53:46 +0800

    Add tag filter and nodemon

--- models/TagTodo.js
+++ models/TagTodo.js
@@ -4,25 +4,20 @@ const db = await connector();
 const table = "Tags";
 
 export default class TagTodo {
-  static create(todo, description, tag) {
-    this.findOne();
-  }
-
-  static edit() {}
-
-  static delete() {}
-
   static async find() {
     const sql = `SELECT * FROM ${table}`;
     const [rows] = await db.execute(sql);
-
     return rows;
   }
 
-  static async findOne(field, value) {
-    const sql = `SELECT * FROM ${table} WHERE ${field} LIKE '%${value}%'`;
+  static async tagsFilter(value) {
+    const sql = `
+      SELECT * FROM ${table} 
+      INNER JOIN Todos
+      ON Tags.id = Todos.tag_id
+      WHERE Tags.id = ${value}
+    `;
     const [rows] = await db.execute(sql);
-
     return rows;
   }
 }
